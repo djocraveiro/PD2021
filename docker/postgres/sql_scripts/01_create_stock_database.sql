ef4542f79473e1c3535dc9a633f5fcb40a210f2d
djocraveiro <djocraveiro@gmail.com>
2021-04-28 22:47:58 +0100

    Add script to create stock_db on container first start.

--- docker/postgres/sql_scripts/01_create_stock_database.sql
+++ docker/postgres/sql_scripts/01_create_stock_database.sql
@@ -1,4 +1,4 @@
-DROP DATABASE if exists dbpeer;
+DROP DATABASE if exists stock_db;
 drop role if exists pguser;
 
 BEGIN;
@@ -6,17 +6,17 @@ CREATE USER pguser WITH encrypted password 'pgpass';
 
 COMMIT;
 
-CREATE DATABASE dbpeer
+CREATE DATABASE stock_db
     WITH 
     OWNER = pguser
     ENCODING = 'UTF8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-\c dbpeer;
+\c stock_db;
 grant usage on schema public to public;
 grant create on schema public to public;
  
-COMMENT ON DATABASE dbpeer IS 'Base de Dados para API Peer Review';
+COMMENT ON DATABASE stock_db IS 'Virtual store stock api database';
 
-GRANT ALL PRIVILEGES ON DATABASE dbpeer TO pguser;
+GRANT ALL PRIVILEGES ON DATABASE stock_db TO pguser;
